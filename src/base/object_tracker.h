e8aa8d05189c623bc146d1f833286dde022784b3
jburnim <jburnim@e75bf3be-2e4d-0410-8328-f5641d7a0f2f>
2009-04-21 00:28:07 +0000

    Adds missing file from previous commit.

---
+++ src/base/object_tracker.h
@@ -0,0 +1,33 @@
+// Copyright (c) 2008, Jacob Burnim (jburnim@cs.berkeley.edu)
+//
+// This file is part of CREST, which is distributed under the revised
+// BSD license.  A copy of this license can be found in the file LICENSE.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See LICENSE
+// for details.
+
+#ifndef BASE_OBJECT_TRACKER_H__
+#define BASE_OBJECT_TRACKER_H__
+
+#include "base/basic_types.h"
+
+namespace crest {
+
+class SymbolicObject;
+
+class ObjectTracker {
+ public:
+  ObjectTracker() { }
+
+  SymbolicObject* find(addr_t addr) const {
+    return NULL;
+  }
+
+ private:
+};
+
+}  // namespace crest
+
+#endif BASE_OBJECT_TRACKER_H__
